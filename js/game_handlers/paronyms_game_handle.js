08aa6add17b9817b00f725b3f188bd4820c92121
Russian EGE <47301367+annprot@users.noreply.github.com>
2019-02-24 22:39:52 +0500

    Fix the bug

--- js/game_handlers/paronyms_game_handle.js
+++ js/game_handlers/paronyms_game_handle.js
@@ -43,6 +43,7 @@ addEventListener('keyup', function () {
 //We divide the task to the blocks
 //Every block contains <= 25 words
 function view_blocks_tasks() {
+	clearTimeout(waiting); //fix waiting for the file
 	if(data.words.length <= 25) start_game();
 	else {
 		var length = data.words.length;
