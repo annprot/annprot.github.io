df0c192411a5bd5b0ea3d771de3878848870bc92
Russian EGE <47301367+annprot@users.noreply.github.com>
2019-05-18 00:31:15 +0500

    Preparing for the update
    - add code for working 1-3 tasks

--- js/game_handlers/1_3_tasks_game_handler.js
+++ js/game_handlers/1_3_tasks_game_handler.js
@@ -9,6 +9,8 @@ var all_elem = 0; //count all emenents in the list
 var h_enter = false; //fix longing enter
 var right_answer = ""; //right answer
 var us_error = false; //don't change the task for user
+var now_task = 0; //state task
+var task_positions = [0, 0, 0];
 
 var first_tasks = new Map();
 var second_tasks = new Map();
@@ -33,7 +35,7 @@ addEventListener("keydown", function(event) {
 		if(h_enter) return;
     h_enter = true;
 
-    if(gameWork) game_handle();
+    if(gameWork) game_handle_list();
 	}
 }, false);
 
@@ -44,6 +46,7 @@ addEventListener('keyup', function () {
 
 //Отображение 1 упражнения
 function show_task_first() {
+	now_task = 1;
 	document.getElementById("buttons").style.display = 'none';
 	document.getElementById("container_task").style.display = 'block';
 	document.getElementById("container_task").innerHTML = '<p class="title_task">Выберите верные утверждения:</p>';
@@ -52,26 +55,242 @@ function show_task_first() {
 	}
 	right_answer = first_tasks.get(texts[0])[first_tasks.get(texts[0]).length - 1];
 	document.getElementById("container_task").innerHTML += '<form onsubmit="return false;" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"><input type="text" id="us_answer"></form>';
-	document.getElementById("container_task").innerHTML += '<button id="game_handle_btn" onclick="game_handle();">Проверить</button>';
+	document.getElementById("container_task").innerHTML += '<button id="game_handle_btn" onclick="game_handle_list();">Проверить</button>';
 	document.getElementById("container_task").innerHTML += '<button id="step_back_btn" onclick="step_back();">Назад</button>';
 }
 
 //Отображение 2 упражнения
 function show_task_second() {
+	now_task = 2;
 	document.getElementById("buttons").style.display = 'none';
 	document.getElementById("container_task").style.display = 'block';
 	document.getElementById("container_task").innerHTML = '<p class="title_task">' + second_tasks.get(texts[0])[0] +'</p>';
-	right_answer = first_tasks.get(texts[0])[first_tasks.get(texts[0]).length - 1];
+	right_answer = second_tasks.get(texts[0])[second_tasks.get(texts[0]).length - 1];
 	document.getElementById("container_task").innerHTML += '<form onsubmit="return false;" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"><input type="text" id="us_answer"></form>';
-	document.getElementById("container_task").innerHTML += '<button id="game_handle_btn" onclick="game_handle();">Проверить</button>';
+	document.getElementById("container_task").innerHTML += '<button id="game_handle_btn" onclick="game_handle_list();">Проверить</button>';
+	document.getElementById("container_task").innerHTML += '<button id="step_back_btn" onclick="step_back();">Назад</button>';
+}
+
+//Отображение 3 упражнения
+function show_task_third() {
+	now_task = 3;
+	document.getElementById("buttons").style.display = 'none';
+	document.getElementById("container_task").style.display = 'block';
+	document.getElementById("container_task").innerHTML = '<p class="title_task">' + third_tasks.get(texts[0])[0] +'</p>';
+	for(var i = 1; i < third_tasks.get(texts[0]).length - 1; i++) {
+		document.getElementById("container_task").innerHTML += '<p class="user_answ">' + third_tasks.get(texts[0])[i] + '</p>';
+	}
+	right_answer = third_tasks.get(texts[0])[third_tasks.get(texts[0]).length - 1];
+	document.getElementById("container_task").innerHTML += '<form onsubmit="return false;" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"><input type="text" id="us_answer"></form>';
+	document.getElementById("container_task").innerHTML += '<button id="game_handle_btn" onclick="game_handle_list();">Проверить</button>';
 	document.getElementById("container_task").innerHTML += '<button id="step_back_btn" onclick="step_back();">Назад</button>';
 }
 
 //Отображаем 3 основные кнопки
 function step_back() {
+	now_task = 0;
 	document.getElementById("container_task").style.display = 'none';
 	document.getElementById("buttons").style.display = 'block';
 }
+
+//Выбираем обработчик
+function game_handle_list() {
+	switch(now_task) {
+		case 1:
+			game_handle_first();
+		break;
+
+		case 2:
+			game_handle_second();
+		break;
+
+		case 3:
+			game_handle_third();
+		break;
+	}
+}
+
+//Button "check" for the 1th task
+function game_handle_first() {
+	var flag = false;
+
+	if(right_answer == document.getElementById("us_answer").value.trim().toLowerCase()) {
+		task_positions[0] = 1;
+		flag = true;
+		document.getElementById("us_answer").style.background = "#57CE79";
+		document.getElementById("us_answer").style.border = "1px solid #57CE79";
+		document.getElementById("us_answer").style.color = "#fff";
+
+		us_error = false;
+
+		setTimeout(function () {
+			document.getElementById("us_answer").placeholder = '';
+			document.getElementById("us_answer").style.background = "none";
+			document.getElementById("us_answer").style.border = "1px solid #fff";
+			document.getElementById("us_answer").style.color = "#fff";
+			document.getElementById("us_answer").value = "";
+
+			document.getElementById("bt_st_1").style.background = '#57CE79';
+			document.getElementById("bt_st_1").style.border = "1px solid #57CE79";
+			document.getElementById("bt_st_1").style.color = "#fff";
+
+			if(!check_processing()) {
+				document.getElementById("container_task").style.display = 'none';
+				document.getElementById("buttons").style.display = 'block';
+			}
+		}, 1000);
+	}
+
+	if(!flag) {
+		//handle mistake
+		document.getElementById("us_answer").value = "";
+		document.getElementById("us_answer").placeholder = right_answer;
+		document.getElementById("us_answer").style.background = "#D63C3C";
+		document.getElementById("us_answer").style.border = "1px solid #D63C3C";
+		document.getElementById("us_answer").style.color = "#fff";
+
+		setTimeout(function () {
+			document.getElementById("us_answer").placeholder = '';
+			document.getElementById("us_answer").style.background = "none";
+			document.getElementById("us_answer").style.border = "1px solid #fff";
+			document.getElementById("us_answer").style.color = "#fff";
+			document.getElementById("us_answer").value = "";
+		}, 1500);
+	} 
+}
+
+//Button "check" for the 2th task
+function game_handle_second() {
+	var flag = false;
+	var r_answer = false;
+
+	try {
+		rights_answers = right_answer.split(",");
+		for(var i = 0; i < rights_answers.length; i++) {
+			if(rights_answers[i] == document.getElementById("us_answer").value.trim().toLowerCase()) r_answer = true;
+		}
+	} catch(e) {
+		if(right_answer == document.getElementById("us_answer").value.trim().toLowerCase()) r_answer = true;
+	}
+
+	if(r_answer) {
+		flag = true;
+		task_positions[1] = 1;
+		document.getElementById("us_answer").style.background = "#57CE79";
+		document.getElementById("us_answer").style.border = "1px solid #57CE79";
+		document.getElementById("us_answer").style.color = "#fff";
+
+		us_error = false;
+
+		setTimeout(function () {
+			document.getElementById("us_answer").placeholder = '';
+			document.getElementById("us_answer").style.background = "none";
+			document.getElementById("us_answer").style.border = "1px solid #fff";
+			document.getElementById("us_answer").style.color = "#fff";
+			document.getElementById("us_answer").value = "";
+
+			document.getElementById("bt_st_2").style.background = '#57CE79';
+			document.getElementById("bt_st_2").style.border = "1px solid #57CE79";
+			document.getElementById("bt_st_2").style.color = "#fff";
+
+			if(!check_processing()) {
+				document.getElementById("container_task").style.display = 'none';
+				document.getElementById("buttons").style.display = 'block';
+			}
+		}, 1000);
+	}
+
+	if(!flag) {
+		//handle mistake
+		document.getElementById("us_answer").value = "";
+		document.getElementById("us_answer").placeholder = right_answer;
+		document.getElementById("us_answer").style.background = "#D63C3C";
+		document.getElementById("us_answer").style.border = "1px solid #D63C3C";
+		document.getElementById("us_answer").style.color = "#fff";
+
+		setTimeout(function () {
+			document.getElementById("us_answer").placeholder = '';
+			document.getElementById("us_answer").style.background = "none";
+			document.getElementById("us_answer").style.border = "1px solid #fff";
+			document.getElementById("us_answer").style.color = "#fff";
+			document.getElementById("us_answer").value = "";
+		}, 1500);
+	} 
+}
+
+//Button "check" for the 3th task
+function game_handle_third() {
+	var flag = false;
+
+	if(right_answer == document.getElementById("us_answer").value.trim().toLowerCase()) {
+		flag = true;
+		task_positions[2] = 1;
+		document.getElementById("us_answer").style.background = "#57CE79";
+		document.getElementById("us_answer").style.border = "1px solid #57CE79";
+		document.getElementById("us_answer").style.color = "#fff";
+
+		us_error = false;
+
+		setTimeout(function () {
+			document.getElementById("us_answer").placeholder = '';
+			document.getElementById("us_answer").style.background = "none";
+			document.getElementById("us_answer").style.border = "1px solid #fff";
+			document.getElementById("us_answer").style.color = "#fff";
+			document.getElementById("us_answer").value = "";
+
+			document.getElementById("bt_st_3").style.background = '#57CE79';
+			document.getElementById("bt_st_3").style.border = "1px solid #57CE79";
+			document.getElementById("bt_st_3").style.color = "#fff";
+
+			if(!check_processing()) {
+				document.getElementById("container_task").style.display = 'none';
+				document.getElementById("buttons").style.display = 'block';
+			}
+		}, 1000);
+	}
+
+	if(!flag) {
+		//handle mistake
+		document.getElementById("us_answer").value = "";
+		document.getElementById("us_answer").placeholder = right_answer;
+		document.getElementById("us_answer").style.background = "#D63C3C";
+		document.getElementById("us_answer").style.border = "1px solid #D63C3C";
+		document.getElementById("us_answer").style.color = "#fff";
+
+		setTimeout(function () {
+			document.getElementById("us_answer").placeholder = '';
+			document.getElementById("us_answer").style.background = "none";
+			document.getElementById("us_answer").style.border = "1px solid #fff";
+			document.getElementById("us_answer").style.color = "#fff";
+			document.getElementById("us_answer").value = "";
+		}, 1500);
+	} 
+}
+
+//Проверяем все ли 3 упражнения сделал пользователь
+function check_processing() {
+	var flag = true;
+
+	for(var i = 0; i < task_positions.length; i++) {
+		if(task_positions[i] == 0) flag = false;
+	}
+
+	//Следующий текст
+	if(flag) {
+		document.getElementById("game").innerHTML = '<p  class="start_tasks" class="par" id="task">Работа с текстом:</p>';
+		document.getElementById("game").innerHTML += '<p  class="start_tasks" class="par" id="text_field"></p>';
+		document.getElementById("game").innerHTML += '<div id="buttons"><button class="start_tasks" id="bt_st_1" onclick="show_task_first()">1</button><button class="start_tasks" id="bt_st_2" onclick="show_task_second()">2</button><button class="start_tasks" id="bt_st_3" onclick="show_task_third()">3</button></div>';
+		document.getElementById("game").innerHTML += '<div id="container_task"></div>';
+		document.getElementById("game").innerHTML += '<p id="counter_1_3">1/6</p>';
+		
+		position++;
+		texts.shift();
+		set_word();
+	} else return false;
+}
+
+
+
 
 //Отображаем сами блоки и динамически распределяем слова
 //Создаем кнопки с блоками
@@ -110,8 +329,9 @@ function start_game() {
 	//ATTENTION! CLASS "start_tasks" IS ONLY FOR THIS TASK
 	document.getElementById("game").innerHTML = '<p  class="start_tasks" class="par" id="task">Работа с текстом:</p>';
 	document.getElementById("game").innerHTML += '<p  class="start_tasks" class="par" id="text_field"></p>';
-	document.getElementById("game").innerHTML += '<div id="buttons"><button id="start_tasks" onclick="show_task_first()">1</button><button id="start_tasks" onclick="show_task_second()">2</button><button id="start_tasks" onclick="show_task_third()">3</button></div>';
+	document.getElementById("game").innerHTML += '<div id="buttons"><button class="start_tasks" id="bt_st_1" onclick="show_task_first()">1</button><button class="start_tasks" id="bt_st_2" onclick="show_task_second()">2</button><button class="start_tasks" id="bt_st_3" onclick="show_task_third()">3</button></div>';
 	document.getElementById("game").innerHTML += '<div id="container_task"></div>';
+	document.getElementById("game").innerHTML += '<p id="counter_1_3">1/6</p>';
 	document.getElementById("header").style.display = "none";
 	document.getElementById("main").style.display = "none";
 	//document.getElementById("task").style.fontWeight = "100";
@@ -154,45 +374,6 @@ function set_data_cut(classname) {
 	set_word();
 }
 
-//Button "check"
-function game_handle() {
-	var flag = false;
-
-	if(right_answer == document.getElementById("us_answer").value) {
-		flag = true;
-		document.getElementById("us_answer").style.background = "#57CE79";
-		document.getElementById("us_answer").style.border = "1px solid #57CE79";
-		document.getElementById("us_answer").style.color = "#fff";
-
-		us_error = false;
-
-		setTimeout(function () {
-			document.getElementById("us_answer").placeholder = '';
-			document.getElementById("us_answer").style.background = "none";
-			document.getElementById("us_answer").style.border = "1px solid #fff";
-			document.getElementById("us_answer").style.color = "#fff";
-			document.getElementById("us_answer").value = "";
-		}, 1000);
-	}
-
-	if(!flag) {
-		//handle mistake
-		document.getElementById("us_answer").value = "";
-		document.getElementById("us_answer").placeholder = right_answer;
-		document.getElementById("us_answer").style.background = "#D63C3C";
-		document.getElementById("us_answer").style.border = "1px solid #D63C3C";
-		document.getElementById("us_answer").style.color = "#fff";
-
-		setTimeout(function () {
-			document.getElementById("us_answer").placeholder = '';
-			document.getElementById("us_answer").style.background = "none";
-			document.getElementById("us_answer").style.border = "1px solid #fff";
-			document.getElementById("us_answer").style.color = "#fff";
-			document.getElementById("us_answer").value = "";
-		}, 1500);
-	} 
-}
-
 //Change the word
 function set_word() {
 	//Start game
@@ -209,12 +390,11 @@ function set_word() {
 
 		gameWork = false;
 		$("#end").fadeIn(1000);
-
 		return;
 	}
 
 	document.getElementById("text_field").innerHTML = texts[0];
-	//document.getElementById("counter").innerHTML = (position + 1) + "/" + all_elem;
+	document.getElementById("counter_1_3").innerHTML = (position + 1) + "/" + all_elem;
 }
 
 
